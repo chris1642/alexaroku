6d148ffcf8691e5a7ee36834a23a2325e090bb3b
chris1642 <cwcusumano@gmail.com>
2016-12-27 23:34:29 -0600

    Added text to remind to replace IP and Port

--- AWS Files - Lambda/serverinfo.js
+++ AWS Files - Lambda/serverinfo.js
@@ -1,2 +1,2 @@
-exports.host = "68.288.282.19"; //this is your public IP - must enable port forwarding. Place within quotation marks (leave the quotes)
-exports.port = 1234; //your forwarded port - no quotation marks
+exports.host = "68.288.282.19"; //Change this YOUR public IP - must enable port forwarding. Place within quotation marks (leave the quotes)
+exports.port = 1234; //Change this to YOUR forwarded port - no quotation marks
