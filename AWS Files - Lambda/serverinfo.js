4e2ba2fec5dcd176500a16873641c6ff4230ba5c
heydabop <heydabop@gmail.com>
2016-12-29 01:19:01 -0600

    add authorization header

--- AWS Files - Lambda/serverinfo.js
+++ AWS Files - Lambda/serverinfo.js
@@ -1,2 +1,3 @@
 exports.host = "68.288.282.19"; //Change this YOUR public IP - must enable port forwarding. Place within quotation marks (leave the quotes)
 exports.port = 1234; //Change this to YOUR forwarded port - no quotation marks
+exports.pass = 'password'; //change this to something random, dont use an actuall password you use for anything else
